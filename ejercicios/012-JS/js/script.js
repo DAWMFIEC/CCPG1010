ef4caa58343f2acd1e471f6de1c3ff0c81ff9085
Allan <aavendan@gmail.com>
2019-06-09 21:29:34 -0500

    JS.pptx vecinos010,011 y 012

---
+++ ejercicios/012-JS/js/script.js
@@ -0,0 +1,101 @@
+function agregarFondo() {
+
+	/* Selecciona el elemento con el id descripcion */
+	/* document.getElementById( ) */
+
+	/* Agrega la clase bg-t80 */
+	/* node.className += */
+	
+}
+
+/* Llama a la función agregarFondo */
+agregarFondo();
+
+function agregarBordes() {
+
+	/* Selecciona los elementos que contienen la clase card */
+	/* document.getElementsByClassName( ) */
+
+	/* Agrega las clases "border-top-0 border-right-0 border-bottom-0 border-info" */
+	
+}
+
+/* Llama a la función agregarBordes */
+agregarBordes();
+
+function agregarBordesRedondeados() {
+
+	/* Selecciona el elemento section#steps div.col-md-4 img */
+	/* document.querySelector( ) */
+	
+
+	/* Agregua la clase rounded-lg */
+	
+	
+}
+
+/* Llama a la función agregarBordesRedondeados */
+
+agregarBordesRedondeados();
+
+function agregarInnerHTML() {
+
+	/* Selecciona el elemento con el id descripcion */
+	/* document.getElementById( ) */
+
+
+	/* Agrega el texto:
+
+	"Vecinos te permite conocer mejor a tus vecinos, a \
+	planear reuniones, administrar las decisiones de \
+	las directivas, a conocer las necesidades de tu \
+	barrio o sector, a conocer los números de emergencias \
+	y mucho más. Somos para estar más unidos."
+
+	*/
+	/* nodo.innerHTML */
+
+
+}
+
+/* Llama a la función agregarInnerHTML */
+
+agregarInnerHTML();
+
+
+function cambiarFooter() {
+
+	/* Selecciona los elementos body y footer */
+	/* document.getElementsByTagName( ) */
+
+	
+
+	/* Elimina la etiqueta footer */
+	
+
+	/* Crea un footer */
+	/* document.createElement( ) */
+	
+
+
+	/* Agrega el texto "COPYRIGHT © 2019. ALL RIGHTS RESERVED TO VECINOS." al footer */
+	/* document.createTextNode( ) */
+	
+
+
+	/* Agrega las clases "py-2 text-center bg-light" */
+	
+
+	/* Agrega el texto al footer */
+	/* node.appendChild */
+	
+
+	/* Agrega el footer al body */
+	/* node.appendChild */
+	
+
+}
+
+/* Llama a la función footer */
+
+cambiarFooter();

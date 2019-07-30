3f0d138c1ebddde9742595f66380c2759b2851dd
Allan <aavendan@gmail.com>
2019-07-30 09:54:04 -0500

    .gitignore

--- ejercicios/024-NoSQL/backend/nosql/config.py
+++ ejercicios/024-NoSQL/backend/nosql/config.py
@@ -3,12 +3,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
 	
-	#MONGODB_DB = "vecinos"
-	#MONGODB_HOST = "localhost"
-	#MONGODB_PORT = 27017
-
 	MONGODB_DB = "vecinos"
-	MONGODB_HOST = "ds255857.mlab.com"
-	MONGODB_PORT = 55857
-	MONGODB_USERNAME = "admin"
-	MONGODB_PASSWORD = "adminadmin1"
+	MONGODB_HOST = "localhost"
+	MONGODB_PORT = 27017
+
+	# MONGODB_DB = "vecinos"
+	# MONGODB_HOST = "ds255857.mlab.com"
+	# MONGODB_PORT = 55857
+	# MONGODB_USERNAME = "admin"
+	# MONGODB_PASSWORD = "adminadmin1"
--- ejercicios/025-Security/xss/main.py
+++ ejercicios/025-Security/xss/main.py
@@ -8,22 +8,22 @@ def index():
 	return render_template('index.html')
 
 
-@app.route('/tweets', methods=['GET', 'POST'])
-def tweet_feed():
-	if request.method == 'POST':
-		tweet = request.form['tweet']
-		tweets.append(tweet)
-	return render_template('tweet_feed.html', tweets=tweets)
-
-# Mitigation
 # @app.route('/tweets', methods=['GET', 'POST'])
 # def tweet_feed():
 # 	if request.method == 'POST':
 # 		tweet = request.form['tweet']
 # 		tweets.append(tweet)
-# 	response = make_response(render_template('tweet_feed.html', tweets=tweets))
-# 	response.headers['Content-Security-Policy'] = "default-src 'self'"
-# 	return response
+# 	return render_template('tweet_feed.html', tweets=tweets)
+
+# Mitigation
+@app.route('/tweets', methods=['GET', 'POST'])
+def tweet_feed():
+	if request.method == 'POST':
+		tweet = request.form['tweet']
+		tweets.append(tweet)
+	response = make_response(render_template('tweet_feed.html', tweets=tweets))
+	response.headers['Content-Security-Policy'] = "default-src 'self'"
+	return response
 
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', port=5000, debug=True)
